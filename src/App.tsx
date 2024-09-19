2548b34ebb10fd6ecf11239e7125c53d6dc9ebf9
Rin Iwai <rin@eyrin.jp>
2024-09-20 05:34:09 +0900

    a

--- src/App.tsx
+++ src/App.tsx
@@ -63,62 +63,54 @@ function App() {
       >
         <IconCurrentLocation size={32} />
       </button>
-      {building && (
+
+      <div className={`${classes.slide} ${building ? classes.slideAnim : ''}`}>
         <div
           style={{
-            position: 'fixed',
-            bottom: 0,
+            maxWidth: '800px',
+            height: '100%',
             width: '100%',
-            height: '45%',
+            backgroundColor: '#ffffff',
+            borderRadius: '1rem 1rem 0 0',
+            padding: '1rem',
+            margin: '0 auto',
+            border: 'solid 1px #aaa',
           }}
         >
-          <div
+          <h2
             style={{
-              maxWidth: '800px',
-              height: '100%',
-              width: '100%',
-              backgroundColor: '#ffffff',
-              borderRadius: '1rem',
-              padding: '1rem',
-              margin: '0 auto',
-              border: 'solid 1px #aaa',
+              fontSize: '1.25rem', // text-xl
+              fontWeight: 'bold', // font-bold
+              marginTop: 0,
+              marginBottom: '1rem', // mb-4
             }}
           >
-            <h2
-              style={{
-                fontSize: '1.25rem', // text-xl
-                fontWeight: 'bold', // font-bold
-                marginTop: 0,
-                marginBottom: '1rem', // mb-4
-              }}
+            {building?.name}
+          </h2>
+          {building?.children.map((child) => (
+            <div
+              key={child.name}
+              style={{ borderRadius: '0.8rem', border: 'solid 1px #aaa' }}
             >
-              {building?.name}
-            </h2>
-            {building?.children.map((child) => (
-              <div
-                key={child.name}
-                style={{ borderRadius: '0.8rem', border: 'solid 1px #aaa' }}
-              >
-                <p>{child.name}</p>
-                {/* <p>{child.description}</p> */}
-              </div>
-            ))}
-            <button
-              type="button"
-              onClick={() => setBuilding(undefined)}
-              style={{
-                marginTop: '1rem', // mt-4
-                padding: '0.5rem', // p-2
-                backgroundColor: '#ef4444', // bg-red-500
-                color: '#ffffff', // text-white
-                borderRadius: '0.25rem', // rounded
-              }}
-            >
-              Close
-            </button>
-          </div>
+              <p>{child.name}</p>
+              {/* <p>{child.description}</p> */}
+            </div>
+          ))}
+          <button
+            type="button"
+            onClick={() => setBuilding(undefined)}
+            style={{
+              marginTop: '1rem', // mt-4
+              padding: '0.5rem', // p-2
+              backgroundColor: '#ef4444', // bg-red-500
+              color: '#ffffff', // text-white
+              borderRadius: '0.25rem', // rounded
+            }}
+          >
+            Close
+          </button>
         </div>
-      )}
+      </div>
     </>
   );
 }
